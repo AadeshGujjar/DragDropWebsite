41c1e2d0e356c05ad9243db97e519f0d87874a90
Aadesh Gujjar <aadesh.gujjar25@gmail.com>
2021-08-12 08:57:53 +0530

    Check

---
+++ dragdrop-web/src/Draggable.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { finished } from 'stream';
+
+
+
+var savedPositions =[];
+
+export default class Draggable extends React.Component{
+    state= {
+        isDragging:false,
+
+        originalX: 0,
+        originalY: 0, //original pos of the element
+
+        translateX: 0, //change in position
+        translateY: 0,
+
+        lastTranslateX: 0,
+        lastTranslateY: 0, //laste place of the element , initially 0
+    };
+    
+
+handleMouseDown = ({clientX , clientY}) =>{
+    window.addEventListener('mousemove', this.handleMouseMove);
+    window.addEventListener('mouseup', this.handleMouseUp);
+
+    this.setState({
+        originalX: clientX,
+        originalY: clientY,
+        isDragging: true,
+    });
+};
+
+
+handleMouseMove = ({clientX , clientY})=>{
+    const {isDragging}=this.state;
+    
+    
+    if(!isDragging){
+        return;
+    }
+
+    this.setState(pervState=>({
+        translateX: clientX - pervState.originalX + pervState.lastTranslateX,
+        translateY: clientY - pervState.originalY + pervState.lastTranslateY,
+    }));
+
+};
+
+
+handleMouseUp=()=>{
+    window.removeEventListener('mousemove',this.handleMouseMove);
+    window.removeEventListener('mouseup',this.handleMouseUp);
+    
+    console.log(this.state.translateX);
+    console.log(this.state.translateY);
+    
+    const finalX=this.state.translateX;
+    const finalY=this.state.translateY;
+    
+    var finalCoods={
+        xCood:finalX,
+        yCood:finalY,
+    }
+
+
+    savedPositions.push(JSON.stringify(finalCoods));
+    console.log(typeof(savedPositions));
+    console.log(savedPositions);
+
+    this.setState({
+        originalX:0,
+        originalY:0,
+        lastTranslateX:finalX,
+        lastTranslateY:finalY,
+        isDragging:false,
+    })
+};
+
+
+
+
+    render() {
+
+        const { children } = this.props;
+        const { translateX, translateY, isDragging } = this.state;
+
+        return(
+            <div
+            onMouseDown={this.handleMouseDown}
+            x={translateX}
+            y={translateY}
+            isDragging={isDragging}
+            style={{transform:`translate(${translateX}px, ${translateY}px)`}}
+            >
+             {children}
+            </div>
+        );
+    }
+}
+
+
